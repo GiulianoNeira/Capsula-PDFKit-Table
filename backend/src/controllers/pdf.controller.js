30c519d8162d99ab1724cad32f2e2a98f97b354b
Giuliano Neira <luis.acuna2101@alumnos.ubiobio.cl>
2024-09-11 22:28:05 -0300

    Cambios en README

--- backend/src/controllers/pdf.controller.js
+++ backend/src/controllers/pdf.controller.js
@@ -2,17 +2,15 @@ import { generatePDF } from "../services/pdf.service.js";
 
 export const generatePDFController = async (req, res) => {
     try {
-        // Llama al servicio que genera el PDF sin pasar la ID
         const [pdfBuffer, error] = await generatePDF();
 
         if (error) {
             return res.status(500).json({ message: error });
         }
 
-        // Si está bien, se envía el PDF generado
         res.setHeader("Content-Type", "application/pdf");
         res.setHeader("Content-Disposition", `attachment; filename="reporte.pdf"`);
-        res.send(pdfBuffer); // pdfBuffer contiene el PDF generado como un Buffer
+        res.send(pdfBuffer); 
     } catch (error) {
         console.error("Error al generar el PDF:", error);
         return res.status(500).json({ message: "Error interno del servidor" });
--- backend/src/services/pdf.service.js
+++ backend/src/services/pdf.service.js
@@ -1,28 +1,22 @@
 import PDFDocument from 'pdfkit-table';
-import fs from 'fs';
-import path from 'path';
-import { v4 as uuidv4 } from 'uuid';
-import { fileURLToPath } from 'url';
-import { getUsersService } from './user.service.js'; // Asegúrate de la ruta correcta
+import { getUsersService } from './user.service.js';
 
 export async function generatePDF() {
     try {
         const doc = new PDFDocument({ margin: 30, size: 'A4' });
         let usersData, error;
-        [usersData, error] = await getUsersService(); // Ajusta según tu lógica
+        [usersData, error] = await getUsersService();
 
         if (error) {
             return [null, error];
         }
 
-        // Crear el PDF en memoria (buffer)
         const pdfBuffer = await new Promise((resolve, reject) => {
             const buffers = [];
             doc.on('data', buffers.push.bind(buffers));
             doc.on('end', () => resolve(Buffer.concat(buffers)));
             doc.on('error', reject);
 
-            // Crear la tabla con los datos de los usuarios
             const table = {
                 title: { label: 'Informe de Usuarios', color: 'blue' },
                 headers: ['id', 'Nombre', 'Email', 'Rut', 'Rol'],
